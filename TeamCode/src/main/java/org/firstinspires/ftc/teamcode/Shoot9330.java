6ba1e76db08a26c8ddaa42a7a80ec0a35b366096
Xavier Cartwright <nemfreed@gmail.com>
2016-10-24 15:57:31 -0700

    Ftc Shooter

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shoot9330.java
@@ -0,0 +1,43 @@
+//Import packages and whatnot
+package org.firstinspires.ftc.teamcode;
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+        import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by robot on 10/17/2016.
+ */
+
+//The shot class
+public class Shoot9330 extends LinearOpMode {
+
+    //Predefine motor shotMotor, the motor used for shooting.
+    DcMotor shotMotor;
+
+    //Empty constructor
+    public Shoot9330 () {
+        //Set the motor
+        shotMotor = hardwareMap.dcMotor.get("shotMotor");
+        //Set the direction of the shootig motor
+        shotMotor.setDirection(DcMotor.direction.REVERSE);
+    }
+
+    //Shoot function
+    public void shoot() {
+        //Wait for start
+        waitForStart();
+
+        //Set the power to max
+        shotMotor.setPower(1);
+        //Wait 1 second
+        sleep(1000);
+        //Set the power to reverse, max
+        shotMotor.setPower(-1);
+        //Wait 1 second
+        sleep(1000);
+        //Stop the motor
+        shotMotor.setPower(0);
+
+    }
+
+}
