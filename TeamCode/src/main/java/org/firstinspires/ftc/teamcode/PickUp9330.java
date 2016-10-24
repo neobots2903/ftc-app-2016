e438c4fe6a86d5f598fd64f7d3b04fa51e8608b6
Xavier Cartwright <nemfreed@gmail.com>
2016-10-24 16:58:45 -0700

    Version 1.0

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PickUp9330.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by robot on 10/17/2016.
+ */
+
+public class PickUp9330 {
+
+    //code to include motor goes here
+
+    //something to make it repeat forever
+
+    boolean pickUpButton; //gamepad button code;
+
+    public void pickup() {
+        if (pickUpButton == true){
+            //spin motor
+        } else {
+            //stop motor
+        }
+    }
+}
+
+//:'(
