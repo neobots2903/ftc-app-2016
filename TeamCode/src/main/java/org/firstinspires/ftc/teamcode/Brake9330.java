4ab1ca3d0bfb67fde5813121e1139fde037aaa7b
Xavier Cartwright <nemfreed@gmail.com>
2016-10-24 16:11:45 -0700

    Updates to Brake9330

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Brake9330.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Robot on 10/24/2016.
+ */
+
+public class Brake9330 {
+
+    /* The brake function will be accessed by both the operator and the robot.
+       The brake function will be achieved by extension from the robot powered by a motor.
+       It will be rotated into a position similar to a kick stand to keep in place
+    */
+
+}
