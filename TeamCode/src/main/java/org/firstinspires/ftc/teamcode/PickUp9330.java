a79fd699878209b63458d6c640daf21beedd310c
Xavier Cartwright <nemfreed@gmail.com>
2016-10-28 17:24:44 -0700

    Fixed pickUp code because of design change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PickUp9330.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PickUp9330.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import static java.lang.Thread.sleep;
+    import com.qualcomm.robotcore.hardware.DcMotor;
+    import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+    import static java.lang.Thread.sleep;
 
 /**
  * Created by robot on 10/17/2016.
@@ -14,38 +15,61 @@ public class PickUp9330 {
 
     //code to include motor goes here
     DcMotor pickUpMotor;
-    boolean pickUpButton; //gamepad button code;
-
-
-    boolean pickUpOpen = false;
+    boolean pickUpButton = gamepad2.button.x; //gamepad button code;
+    boolean pickUpEjectButton = gamepad2.button.y; //gamepad button code;
 
     //Empty constructor
-    public PickUp9330 (Hardware9330 robotMap) {
+    public PickUp9330(Hardware9330 robotMap) {
         //Set the motor
-//        shotMotor = Hardware9330.dcMotor.get("shotMotor");
-        //Set the direction of the shootig motor
+//        pickUpMotor = Hardware9330.dcMotor.get("pickUpMotor");
+        //Set the direction of the pickup motor
         hwMap = robotMap;
         hwMap.pickUpMotor.setDirection(DcMotor.Direction.REVERSE);
     }
 
-    public void pickup() throws InterruptedException{
-        if (pickUpButton == true){
-
-            //open "scissors" (motor power + and stop)
-            pickUpMotor.setPower(1); //turn on motor
-            sleep(1000); //time in milliseconds motor is running
-            pickUpOpen = true;
-
-            while (pickUpButton == true && pickUpOpen) {
-                pickUpMotor.setPower(0); //stop motor and keep it open
-            }
-
+    public void pickUp() {
+        if (pickUpButton == true) {
+            pickUpMotor.setPower(1);
         } else {
-            //keep "scissors" closed (motor power - constantly)
-            pickUpMotor.setPower(-1);
-            pickUpOpen = false;
+            pickUpMotor.setPower(0);
         }
     }
-}
 
-//:'(  I have so much more to do
+    public void pickUpEject() {
+        if (pickUpEjectButton == true) {
+            pickUpMotor.setPower(-1);
+        }
+    }
+
+    /** <-- delete this to uncomment
+
+     This was going to be code for pickup, but design was changed. Keeping just in case.
+
+     public void pickUpOpen() throws InterruptedException {
+     if (hwMap != null) {
+     //open "scissors" (motor power + and stop)
+     pickUpMotor.setPower(1); //turn on motor
+     sleep(1000); //time in milliseconds motor is running
+     pickUpMotor.setPower(0); //stops motor
+     }
+     }
+
+     public void pickUpClose() {
+     if (hwMap != null) {
+     //keep "scissors" closed (motor power - constantly)
+     pickUpMotor.setPower(-1);
+     }
+     }
+
+     public void pickup() throws InterruptedException {
+     if (pickUpButton == true) {
+     pickUpOpen();
+     while (pickUpButton == true) {
+     pickUpMotor.setPower(0);
+     }
+     } else {
+     pickUpClose();
+     }
+     }
+     **/
+}
