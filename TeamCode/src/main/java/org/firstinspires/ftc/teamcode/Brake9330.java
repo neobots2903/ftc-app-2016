4bccdbac2b5d064b9d9736462c98d5fdc747f5be
Xavier Cartwright <nemfreed@gmail.com>
2016-10-29 12:32:21 -0700

    Added fuctionality to Brake9330.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Brake9330.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Brake9330.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.hardware.Servo;
+
 /**
  * Created by Robot on 10/24/2016.
  */
@@ -10,11 +10,35 @@ public class Brake9330  {
        The brake function will be achieved by extension from the robot powered by a motor.
        It will be rotated into a position similar to a kick stand to keep in place.
     */
+private boolean brakeEngaged;
 
-
-    static final Double MIN_POS = 0.0;
-    static final Double MAX_POS = 1.0;
+//   Declaring Set positions for servo
+    static final Double RELEASE_POS = 0.0;
+    static final Double ENGAGED_POS = 1.0;
+//   accessing Hardware9330
     private Hardware9330 hwMap = null;
-   }
+
+    public Brake9330 (Hardware9330 robotMap){
+
+        hwMap = robotMap;
+        hwMap.brake.setPosition(RELEASE_POS);
+        brakeEngaged = false;
+    }
+
+    public void EngageBrake(){
+        if(!brakeEngaged){
+            brakeEngaged = true;
+            hwMap.brake.setPosition(ENGAGED_POS);
+        }
+    }
+
+    public void releaseBrake(){
+        if(brakeEngaged){
+            brakeEngaged = false;
+            hwMap.brake.setPosition(RELEASE_POS);
+        }
+    }
+
+}
 
 
