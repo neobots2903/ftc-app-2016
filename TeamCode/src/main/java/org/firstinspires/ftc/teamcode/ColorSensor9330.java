f7af125aa84faa25745ca6f9ff6de504aec46388
Xavier Cartwright <nemfreed@gmail.com>
2016-10-31 16:05:13 -0700

    Edit ColorSensor9330

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensor9330.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensor9330.java
@@ -5,4 +5,27 @@ package org.firstinspires.ftc.teamcode;
  */
 
 public class ColorSensor9330 {
+
+    ColorSensor csensor;
+
+    public ColorSensor9330() {
+        csensor = hardwareMap.colorSensor.get("sensor_color");
+        csensor.enableLed(bLedOn);
+    }
+
+    public float r() {
+        waitForStart();
+        return csensor.red();
+    }
+
+    public float g() {
+        waitForStart();
+        return csensor.green();
+    }
+
+    public float b() {
+        waitforStart();
+        return csensor.blue();
+    }
+
 }
