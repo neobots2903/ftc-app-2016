49c1ce35f2f628eb2bd1f9ae2fe2df491c1e8959
Xavier Cartwright <nemfreed@gmail.com>
2016-11-04 16:22:06 -0700

    BBoop Finished

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBoop9330.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBoop9330.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 /**
  * Created by NeoBOTS on 10/17/2016.
@@ -8,10 +9,31 @@ public class BBoop9330 {
     public Hardware9330 hwMap = null;
     static final double LEFT_POS = 1.0;
     static final double RIGHT_POS = 0.0;
+    ColorSensor csensor;
 
     public BBoop9330(Hardware9330 robotMap) {
-    hwMap = robotMap;
+        hwMap = robotMap;
         hwMap.beBoop.setPosition(RIGHT_POS);
+        csensor = hwMap.CSensor;
+        csensor.enableLed(true);
+    }
+
+    public void swivelRedLeft() {
+        if (csensor.red() > 200) {
+            hwMap.beBoop.setPosition(LEFT_POS);
+        }
+        if (csensor.blue() > 200) {
+            hwMap.beBoop.setPosition(RIGHT_POS);
+        }
+    }
+
+    public void swivelRedRight() {
+        if (csensor.red() > 200) {
+            hwMap.beBoop.setPosition(RIGHT_POS);
+        }
+        if (csensor.blue() > 200) {
+            hwMap.beBoop.setPosition(LEFT_POS);
+        }
     }
 
 }
