8d9dfc6efcbf96184abdd0f075ab5b8f2cbc32d5
Xavier Cartwright <nemfreed@gmail.com>
2016-11-04 17:37:02 -0700

    added line9330 class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Line9330.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+
+/**
+ * Created by robot on 11/4/2016.
+ */
+
+public class Line9330 {
+    public Hardware9330 hwMap = null;
+    ColorSensor csensor;
+
+    public Line9330(Hardware9330 robotMap) {
+        hwMap = robotMap;
+        csensor = hwMap.CSensor;
+        csensor.enableLed(true);
+    }
+
+}
