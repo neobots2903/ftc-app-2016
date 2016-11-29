f157f67dbed5923f4a3966f36d4e8e58eda684ce
Xavier Cartwright <nemfreed@gmail.com>
2016-11-28 17:10:57 -0800

    prepared it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto9330Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto9330Shooter.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 /**
  * Created by NeoBOTS on 11/21/2016.
  */
-@Autonomous(name= "auto9330WithGyro", group ="Opmode")
+@Autonomous(name= "auto9330Shooter", group ="Opmode")
 public class auto9330Shooter extends LinearOpMode {
 
     @Override
