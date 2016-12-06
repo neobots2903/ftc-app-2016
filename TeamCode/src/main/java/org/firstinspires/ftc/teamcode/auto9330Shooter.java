a7fd4b1d1ad41bd26c74b68735d2760bdb6dcb72
Xavier Cartwright <nemfreed@gmail.com>
2016-12-05 16:28:08 -0800

    Almost finished auto9330Shooter - not yet tested and still contains one error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto9330Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto9330Shooter.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * Created by NeoBOTS on 11/21/2016.
@@ -9,9 +10,22 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name= "auto9330Shooter", group ="Opmode")
 public class auto9330Shooter extends LinearOpMode {
 
+    Hardware9330 robot9330 = new Hardware9330();
+    private ElapsedTime runtime = new ElapsedTime();
+    drive9330 ds = new drive9330();
+    Shoot9330 shooter = new Shoot9330();
+
     @Override
     public void runOpMode() throws InterruptedException {
+        robot9330.init(hardwareMap);
+        ds.init(robot9330);
+        Brake9330 brake = new Brake9330(robot9330);
+        brake.releaseBrake();
 
+        wait(10000);
+        ds.drive(1000);
+
+        shooter.shoot();
     }
 
 
