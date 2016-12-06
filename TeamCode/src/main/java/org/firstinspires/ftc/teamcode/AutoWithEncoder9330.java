230a8d542640ee98411c66bc2d6920c60b84f6b4
Xavier Cartwright <nemfreed@gmail.com>
2016-12-05 16:39:58 -0800

    Changed wheels size

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWithEncoder9330.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWithEncoder9330.java
@@ -20,7 +20,7 @@ public class AutoWithEncoder9330 extends LinearOpMode {
 
     static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     WHEEL_DIAMETER_INCHES   = 3.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.14159);
     static final double     DRIVE_SPEED             = 0.6;
