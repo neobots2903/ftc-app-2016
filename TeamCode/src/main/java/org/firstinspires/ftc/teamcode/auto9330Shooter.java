fc6baf32da4fa14dd219fe87471ec989eae3bee7
Xavier Cartwright <nemfreed@gmail.com>
2016-12-05 18:06:29 -0800

    Done - just need to fix error (and probably change shoot...)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto9330Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto9330Shooter.java
@@ -13,7 +13,7 @@ public class auto9330Shooter extends LinearOpMode {
     Hardware9330 robot9330 = new Hardware9330();
     private ElapsedTime runtime = new ElapsedTime();
     drive9330 ds = new drive9330();
-    //Shoot9330 shooter = new Shoot9330();
+    Shoot9330 shooter = new Shoot9330();
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -25,7 +25,9 @@ public class auto9330Shooter extends LinearOpMode {
         wait(10000);
         ds.drive(1000);
 
-        //shooter.shoot();
+        shooter.shoot();
+
+        ds.drive(750);
     }
 
 
